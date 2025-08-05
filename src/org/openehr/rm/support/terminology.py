8275126a886e1018c47c30f8566250d8f09c87fd
Henry Wright <39097403+henrydwright@users.noreply.github.com>
2025-08-05 20:55:20 +0100

    Implemented rm.support 5.4.4 OPENEHR_TERMINOLOGY_GROUP_IDENTIFIERS

--- src/org/openehr/rm/support/terminology.py
+++ src/org/openehr/rm/support/terminology.py
@@ -22,3 +22,43 @@ class OpenEHRCodeSetIdentifiers:
             (an_id == OpenEHRCodeSetIdentifiers.CODE_SET_ID_MEDIA_TYPES) or
             (an_id == OpenEHRCodeSetIdentifiers.CODE_SET_ID_NORMAL_STATUSES)
         )
+    
+class OpenEHRTerminologyGroupIdentifiers:
+    """List of identifiers for groups in the openEHR terminology."""
+
+    TERMINOLOGY_ID_OPENEHR = "openehr"
+    """Name of openEHR's own terminology."""
+
+    GROUP_ID_AUDIT_CHANGE_TYPE = "audit change type"
+    GROUP_ID_ATTESTATION_REASON = "attestation reason"
+    GROUP_ID_COMPOSITION_CATEGORY = "composition category"
+    GROUP_ID_EVENT_MATH_FUNCTION = "event math function"
+    GROUP_ID_INSTRUCTION_STATES = "instruction states"
+    GROUP_ID_INSTRUCTION_TRANSITIONS = "instruction transitions"
+    GROUP_ID_NULL_FLAVOURS = "null flavours"
+    GROUP_ID_PROPERTY = "property"
+    GROUP_ID_PARTICIPATION_FUNCTION = "participation function"
+    GROUP_ID_SETTING = "setting"
+    GROUP_ID_TERM_MAPPING_PURPOSE = "term mapping purpose"
+    GROUP_ID_SUBJECT_RELATIONSHIP = "subject relationship"
+    GROUP_ID_VERSION_LIFE_CYCLE_STATE = "version lifecycle state"
+
+    # TODO: report typo in spec where an_id is a boolean - https://specifications.openehr.org/releases/RM/Release-1.1.0/support.html#_openehr_code_set_identifiers_class
+    def valid_terminology_group_id(an_id : str) -> bool:
+        """Validity function to test if an identifier is in the set defined by this class."""
+        return (
+            (an_id == OpenEHRTerminologyGroupIdentifiers.TERMINOLOGY_ID_OPENEHR) or
+            (an_id == OpenEHRTerminologyGroupIdentifiers.GROUP_ID_AUDIT_CHANGE_TYPE) or
+            (an_id == OpenEHRTerminologyGroupIdentifiers.GROUP_ID_ATTESTATION_REASON) or
+            (an_id == OpenEHRTerminologyGroupIdentifiers.GROUP_ID_COMPOSITION_CATEGORY) or
+            (an_id == OpenEHRTerminologyGroupIdentifiers.GROUP_ID_EVENT_MATH_FUNCTION) or
+            (an_id == OpenEHRTerminologyGroupIdentifiers.GROUP_ID_INSTRUCTION_STATES) or
+            (an_id == OpenEHRTerminologyGroupIdentifiers.GROUP_ID_INSTRUCTION_TRANSITIONS) or
+            (an_id == OpenEHRTerminologyGroupIdentifiers.GROUP_ID_NULL_FLAVOURS) or
+            (an_id == OpenEHRTerminologyGroupIdentifiers.GROUP_ID_PROPERTY) or
+            (an_id == OpenEHRTerminologyGroupIdentifiers.GROUP_ID_PARTICIPATION_FUNCTION) or
+            (an_id == OpenEHRTerminologyGroupIdentifiers.GROUP_ID_SETTING) or
+            (an_id == OpenEHRTerminologyGroupIdentifiers.GROUP_ID_TERM_MAPPING_PURPOSE) or
+            (an_id == OpenEHRTerminologyGroupIdentifiers.GROUP_ID_SUBJECT_RELATIONSHIP) or
+            (an_id == OpenEHRTerminologyGroupIdentifiers.GROUP_ID_VERSION_LIFE_CYCLE_STATE)
+        )
--- test/org/openehr/rm/support/terminology_test.py
+++ test/org/openehr/rm/support/terminology_test.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.org.openehr.rm.support.terminology import OpenEHRCodeSetIdentifiers
+from src.org.openehr.rm.support.terminology import OpenEHRCodeSetIdentifiers, OpenEHRTerminologyGroupIdentifiers
 
 def test_valid_code_set_id_returns_true_for_valid_ids():
     valid_ids = [
@@ -31,3 +31,45 @@ def test_valid_code_set_id_returns_false_for_invalid_ids():
     ]
     for code_set_id in invalid_ids:
         assert OpenEHRCodeSetIdentifiers.valid_code_set_id(code_set_id) is False
+        
+def test_valid_terminology_group_id_returns_true_for_valid_ids():
+    valid_ids = [
+        OpenEHRTerminologyGroupIdentifiers.TERMINOLOGY_ID_OPENEHR,
+        OpenEHRTerminologyGroupIdentifiers.GROUP_ID_AUDIT_CHANGE_TYPE,
+        OpenEHRTerminologyGroupIdentifiers.GROUP_ID_ATTESTATION_REASON,
+        OpenEHRTerminologyGroupIdentifiers.GROUP_ID_COMPOSITION_CATEGORY,
+        OpenEHRTerminologyGroupIdentifiers.GROUP_ID_EVENT_MATH_FUNCTION,
+        OpenEHRTerminologyGroupIdentifiers.GROUP_ID_INSTRUCTION_STATES,
+        OpenEHRTerminologyGroupIdentifiers.GROUP_ID_INSTRUCTION_TRANSITIONS,
+        OpenEHRTerminologyGroupIdentifiers.GROUP_ID_NULL_FLAVOURS,
+        OpenEHRTerminologyGroupIdentifiers.GROUP_ID_PROPERTY,
+        OpenEHRTerminologyGroupIdentifiers.GROUP_ID_PARTICIPATION_FUNCTION,
+        OpenEHRTerminologyGroupIdentifiers.GROUP_ID_SETTING,
+        OpenEHRTerminologyGroupIdentifiers.GROUP_ID_TERM_MAPPING_PURPOSE,
+        OpenEHRTerminologyGroupIdentifiers.GROUP_ID_SUBJECT_RELATIONSHIP,
+        OpenEHRTerminologyGroupIdentifiers.GROUP_ID_VERSION_LIFE_CYCLE_STATE,
+    ]
+    for group_id in valid_ids:
+        assert OpenEHRTerminologyGroupIdentifiers.valid_terminology_group_id(group_id) is True
+
+def test_valid_terminology_group_id_returns_false_for_invalid_ids():
+    invalid_ids = [
+        "",
+        "openehr terminology",
+        "audit_change_type",
+        "attestation reason ",
+        "composition-category",
+        "event math",
+        "instruction state",
+        "null flavour",
+        "property ",
+        "participation function1",
+        "setting_type",
+        "term mapping",
+        "subject relationship ",
+        "version lifecycle",
+        None,
+        456,
+    ]
+    for group_id in invalid_ids:
+        assert OpenEHRTerminologyGroupIdentifiers.valid_terminology_group_id(group_id) is False
