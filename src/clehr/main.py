742e45b0101b59311e57164555417d9fef632470
Henry Wright <39097403+henrydwright@users.noreply.github.com>
2025-08-11 00:20:29 +0100

    Added example concrete implementation of MEASUREMENT_SERVICE

--- src/clehr/main.py
+++ src/clehr/main.py
@@ -1,9 +1,11 @@
 from terminology import CSVTerminologyService
+from measurement import NLMMeasurementService
 from org.openehr.rm.data_types.text import CodePhrase
 
 print("Welcome to clehr - using pyehr, a Python implemention of OpenEHR")
 
-ts = CSVTerminologyService("ICD10(2020)", "data/icd10_2020.csv", "en")
+ts = CSVTerminologyService("openehr(3)", "data/openehr-en2.csv", "en")
+ms = NLMMeasurementService()
 
 def print_list_strs(lst):
     for item in lst:
@@ -15,7 +17,7 @@ while True:
     if user_command_parts[0] == "exit":
         break
     elif user_command_parts[0] == "term":
-        te = ts.terminology("ICD10(2020)")
+        te = ts.terminology("openehr(3)")
         res = []
         if len(user_command_parts) == 1:
             res = te.all_codes()
@@ -28,7 +30,21 @@ while True:
                 except ValueError as ve:
                     res = [str(ve)]
         else:
-            res = ["Invalid use of terminology. Expected terminology, terminology group <group> or terminology code <code>"]
+            res = ["Invalid use of terminology. Expected term, term group <group> or term code <code>"]
         print_list_strs(res)
+    elif user_command_parts[0] == "measure":
+        MEASURE_ERROR = "Invalid use of measure. Expected measure valid <unit> or measure equiv <unit1> <unit2>"
+        if len(user_command_parts) == 3:
+            if user_command_parts[1] == "valid":
+                print(ms.is_valid_units_string(user_command_parts[2]))
+            else:
+                print(MEASURE_ERROR)
+        elif len(user_command_parts) == 4:
+            if user_command_parts[1] == "equiv":
+                print(ms.units_equivalent(user_command_parts[2], user_command_parts[3]))
+            else:
+                print(MEASURE_ERROR)
+        else:
+            print(MEASURE_ERROR)
     else:
         print("Command not recognised.")
--- src/org/openehr/rm/support/measurement.py
+++ src/org/openehr/rm/support/measurement.py
@@ -7,11 +7,11 @@ class MeasurementService(ABC):
         super().__init__()
 
     @abstractmethod
-    def is_valid_units_string(units: str) -> bool:
+    def is_valid_units_string(self, units: str) -> bool:
         """True if the units string 'units' is a valid string according to the HL7 UCUM specification."""
         pass
 
     @abstractmethod
-    def units_equivalent(units1: str, units2: str) -> bool:
+    def units_equivalent(self, units1: str, units2: str) -> bool:
         """True if two units strings correspond to the same measured property."""
         pass
