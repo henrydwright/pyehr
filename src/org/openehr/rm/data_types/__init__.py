c0fad3e07280a626fa223526da57d36f3bb54e2d
Henry Wright <39097403+henrydwright@users.noreply.github.com>
2025-08-12 21:29:21 +0100

    Implement rm.data_types DV_BOOLEAN DV_STATE and DV_IDENTIFIER

--- src/org/openehr/rm/data_types/__init__.py
+++ src/org/openehr/rm/data_types/__init__.py
@@ -4,6 +4,7 @@ from org.openehr.base.foundation_types.any import AnyClass
 from org.openehr.base.base_types.definitions import OpenEHRDefinitions
 
 class DataValue(ABC, OpenEHRDefinitions):
+    """Abstract parent of all DV_ data value types. Serves as a common ancestor of all data value types in openEHR models."""
     
     @abstractmethod
     def __init__(self):
